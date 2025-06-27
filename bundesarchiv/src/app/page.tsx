b8584f62b6d238272bdb6d544674c150e273c4d2
ZachariasFrings <frinzach@gmail.com>
2025-06-27 23:58:24 +0200

    begun setting up project and adding Dockerfile

---
+++ bundesarchiv/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button, HStack, Input } from "@chakra-ui/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <HStack>
+      <Input></Input>
+      <Button>Click me</Button>
+    </HStack>
+      );
+}
